b41435c3b57b0323176f3dd311996b8e8cc58c53
mhoang17 <mhoang17@github.com>
2019-12-16 23:10:10 +0100

    - Split main into a function for itself

--- test/ComplexTest.py
+++ test/ComplexTest.py
@@ -1,62 +1,8 @@
-import time
 from gensim.models import Word2Vec
-from KnowledgeGraph import Relevance, KGTraversal, PathProcessing
+from KnowledgeGraph import Relevance, KGTraversal
 from PredicatesEnum import PredicatesEnum as PreEnum
-from QueryProcesing import WordToKey
 from utils import Dictionaries
-
-
-# The main run through
-def run_main(en_pred_list):
-
-    names = []
-    entities_list = []
-    predicates = []
-
-    for elem in en_pred_list:
-        # We know that idx 0 of the a list in the 'en_pred_list' is the name of the entity
-        string = str(elem[0])
-        names.append(string)
-
-        # Get the key values of the entities (tt..... : 'Kevin Hart')
-        keys = WordToKey.key_value(string, dictionary)
-        entities_list.append(keys)
-
-        # We know that idx 1 of the a list in the 'en_pred_list' is the predicates belonging to the entity
-        predicates.append(elem[1])
-
-    all_paths = []
-    predicate_idx = 0
-
-    # There are many people or titles that can be named the same, therefore we go through all of them and check
-    # which relations they have
-    for entities in entities_list:
-        for entity in entities:
-            # List of already visited notes so we don't re-visit them
-            already_visited = [entity]
-
-            # We run the current key through the function that does the graph traversal
-            results = KGTraversal.find_all_paths(kg_graph, entity, predicates[predicate_idx], already_visited, 0)
-
-            if results:
-                # Translate the keys to corresponding their names/values
-                translated_results = []
-                for result in results:
-
-                    if result not in PreEnum._value2member_map_:
-                        translated_results.append(dictionary[result])
-                    else:
-                        translated_results.append(result)
-
-                all_paths.append(translated_results)
-
-        # When we have found all paths for one entity, we need to move to the next entry in 'predicates'
-        predicate_idx += 1
-
-    sec_all_paths = PathProcessing.path_sectioning(all_paths)
-    paths_list = PathProcessing.seperate_all_paths(predicates, sec_all_paths, names)
-
-    return paths_list
+from Main import UserSearch
 
 
 # Load model
@@ -80,7 +26,7 @@ entity_predicates = [["M. Night", [PreEnum.DIRECTED.value, PreEnum.HAS_ACTOR.val
                      ["X-Men", [PreEnum.HAS_ACTOR.value, PreEnum.STARRED_IN.value]]]
 
 # Find all paths
-paths = run_main(entity_predicates)
+paths = UserSearch.find_all_paths(entity_predicates, dictionary, kg_graph)
 
 # Find the best results
 best_results = Relevance.most_relevant_path(paths, question, model)
--- test/ExecuteTest.py
+++ test/ExecuteTest.py
@@ -2,70 +2,17 @@ import time
 import pandas as pd
 import traceback
 from random import choice
-from KnowledgeGraph import KGTraversal, PathProcessing, Relevance
-from QueryProcesing import QueryAnalysis, WordToKey
+from KnowledgeGraph import KGTraversal, Relevance
+from QueryProcesing import QueryAnalysis
 from utils import Dictionaries
 from gensim.models import Word2Vec
-from PredicatesEnum import PredicatesEnum as PreEnum
+from Main import UserSearch
 
 
 # Load training data
 def load_files():
     file = pd.read_csv('wiki-entities_qa_actor_to_movie_dev.csv', header=None, sep="\t")
     return file
-
-
-# The main run through
-def run_main(en_pred_list):
-
-    names = []
-    entities_list = []
-    predicates = []
-
-    for elem in en_pred_list:
-        # We know that idx 0 of the a list in the 'en_pred_list' is the name of the entity
-        string = str(elem[0])
-        names.append(string)
-
-        # Get the key values of the entities (nm..... : 'Kevin Hart')
-        keys = WordToKey.key_value(string, dictionary)
-        entities_list.append(keys)
-
-        # We know that idx 1 of the a list in the 'en_pred_list' is the predicates belonging to the entity
-        predicates.append(elem[1])
-
-    all_paths = []
-    predicate_idx = 0
-
-    # There are many people or titles that can be named the same, therefore we go through all of them and check
-    # which relations they have
-    for entities in entities_list:
-        for entity in entities:
-            # List of already visited notes so we don't re-visit them
-            already_visited = [entity]
-
-            # We run the current key through the function that does the graph traversal
-            results = KGTraversal.find_all_paths(kg_graph, entity, predicates[predicate_idx], already_visited, 0)
-
-            if results:
-                # Translate the keys to corresponding their names/values
-                translated_results = []
-                for result in results:
-
-                    if result not in PreEnum._value2member_map_:
-                        translated_results.append(dictionary[result])
-                    else:
-                        translated_results.append(result)
-
-                all_paths.append(translated_results)
-
-        # When we have found all paths for one entity, we need to move to the next entry in 'predicates'
-        predicate_idx += 1
-
-    sec_all_paths = PathProcessing.path_sectioning(all_paths)
-    paths_list = PathProcessing.seperate_all_paths(predicates, sec_all_paths, names)
-
-    return paths_list
 
 
 # Randomize Paths
@@ -84,35 +31,35 @@ def randomize_paths(paths):
 
 
 # Count, calculate percentages and write to file
-def count_cal_write(answers, top, file):
+def count_cal_write(cand_answer, top, file):
     count = 0
     placement = 0
     placement_list = []
 
-    for answer in answers:
+    for answer in cand_answer:
         if answer in top:
             placement += top.index(answer) + 1
             placement_list.append(top.index(answer) + 1)
             count += 1
 
     if count != 0:
-        percentage = count / len(answers)
+        percentage = count / len(cand_answer)
         placement = placement / count
     else:
         percentage = 0.0
 
     print("Percentage: ", percentage, " Question: ", question)
-    print("Their answers: ", answers)
+    print("Their answers: ", cand_answer)
     print("Our answers: ", top)
     print("Placements: ", placement_list, " Average placement: ", placement)
 
     file.write(
-        question + "\t" + str(len(answers)) + "\t" + str(len(top)) + "\t"
+        question + "\t" + str(len(cand_answer)) + "\t" + str(len(top)) + "\t"
         + str(percentage) + "\t" + str(placement_list) + "\t" + str(placement) + "\n")
 
 
 # Test knowledge graph
-def test_kg(answers, paths, file):
+def test_kg(cand_answer, paths, file):
 
     # List of top 10
     top_kg = []
@@ -126,26 +73,25 @@ def test_kg(answers, paths, file):
         for path in paths:
             top_kg.append(path[len(path) - 1])
 
-    count_cal_write(answers, top_kg, file)
+    count_cal_write(cand_answer, top_kg, file)
 
 
 # Test relevance
-def test_relevance(answers, paths, file, model, query):
-    best_results = Relevance.most_relevant_path(paths, query, model)
+def test_relevance(cand_answer, paths, file, w2v_model, query):
+    best_results = Relevance.most_relevant_path(paths, query, w2v_model)
 
     top_results = []
 
     for element in best_results:
         top_results.append(element[len(element) - 1])
 
-    count_cal_write(answers, top_results, file)
+    count_cal_write(cand_answer, top_results, file)
 
 
 # Load test data
 test_dataset = load_files()
 
-# Files load
-#test_results_kg = open("test.csv", "w+")
+# test_results_kg = open("test.csv", "w+")
 test_results = open("test.csv", "w+")
 time_results = open("test_time.csv", "w+")
 
@@ -178,7 +124,7 @@ for question in test_dataset[0]:
         entity_predicate_list = QueryAnalysis.query_processing(question)
 
         # Finds paths
-        paths_list = run_main(entity_predicate_list)
+        paths_list = UserSearch.find_all_paths(entity_predicate_list, dictionary, kg_graph)
 
         # Comment this in if we want to randomize paths
         # paths_list = randomize_paths(paths_list)
@@ -203,7 +149,7 @@ for question in test_dataset[0]:
         traceback.print_exc()
         continue
 
-#test_results_kg.close()
+# test_results_kg.close()
 test_results.close()
 time_results.close()
 
